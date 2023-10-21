210926e7396d931151750b3cfb7dae2c6f371710
Clément Salomon <114286471+ClementRKG@users.noreply.github.com>
2023-10-22 00:25:48 +0200

    Afficher un dialogue (html+css)

--- app/controllers/Intro.php
+++ app/controllers/Intro.php
@@ -3,7 +3,8 @@ class IntroController
 {
     public function execute(): void
     {
-        (new IntroView())->show();
+        $text = "Hey, <br> j’ai pris l’habitude de lire le journal dans le parc après mon boulot. <br> Cependant, j’ai récemment remarqué que de plus en plus d’articles sonnaient “faux” ou peu fiables. <br> Pourrais tu m’aider à les trouver ?";
+        (new IntroView())->show('adult', $text);
     }
 
 }
--- app/views/Intro.php
+++ app/views/Intro.php
@@ -1,23 +1,41 @@
 <?php
 class IntroView
 {
-    public function show(): void
+    public function show($chara, $text): void
     {
         ob_start();
 ?>
 
 <div id="bas">
 
-    <section id="bulle_nom">
-        <h1>Moi</h1>
-    </section>
+    <div id="character">
+        <section id="bulle_image">
+            <?php
+                switch ($chara) {
+                    case 'young':
+                        echo "<img id='head' src='/assets/images/characters/young/young_head.png' alt='Head of the talking character'>";
+                        break;
+                    case 'adult':
+                        echo "<img id='head' src='/assets/images/characters/adult/adult_head.png' alt='Head of the talking character'>";
+                        break;
+                    case 'old':
+                        echo "<img id='head' src='/assets/images/characters/old/old_head.png' alt='Head of the talking character'>";
+                        break;
+                    default:
+                        echo "<img id='head' src='/assets/images/characters/me_head.png' alt='Head of the talking character'>";
+                        break;
+                }
+            ?>
+        </section>
+        <section id="bulle_nom">
+            <h1> <?php echo $chara ?> </h1>
+        </section>
+    </div>
 
     <section id="bulle_texte">
-        <h2> Texte </h2>
-        <h2> Texte </h2>
-        <h2> Texte </h2>
-        <h2> Texte </h2>
-        <h2> Texte </h2>
+        <p>
+            <?php echo $text ?>
+        </p>
     </section>
 
 </div>
