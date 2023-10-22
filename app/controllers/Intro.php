609a9a5ffea6e0e8b92312aada8a357c7a0d0cb5
Edouard-Pon <edouard.ponomarenko@gmail.com>
2023-10-22 04:15:06 +0200

    Changed text in Intro

--- app/controllers/Intro.php
+++ app/controllers/Intro.php
@@ -12,7 +12,7 @@ class IntroController
             'character_type' => 'adult',
             'character_head' => 'adult_head',
             'character_name' => 'Adult',
-            'text' => 'Hello my friend!'
+            'text' => 'Hey, <br> j’ai pris l’habitude de lire le journal dans le parc après mon boulot. <br> Cependant, j’ai récemment remarqué que de plus en plus d’articles sonnaient “faux” ou peu fiables. <br> Pourrais tu m’aider à les trouver ?'
         ];
         // example of $dataListener, remove sooner
         $dataListener = [
