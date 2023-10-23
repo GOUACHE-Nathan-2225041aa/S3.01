9ff6df2842c75e24a6dc54bfc4cd56c32c89df8c
NathanGOUACHEG22017449 <nathan.gouache@etu.univ-amu.fr>
2023-10-23 14:44:35 +0200

    On dégage le vieux WIP qui servait à rien

--- app/views/Young.php
+++ app/views/Young.php
@@ -7,11 +7,6 @@ class YoungView
     public function show($imgUrl): void
     {
         ob_start();
-?>
-<div>
-    WIP
-</div>
-<?php
 include('DeepFakeGUI.php');
         (new Layout('FakeGame - Young', ob_get_clean(), 'youngGame'))->show();
     }
