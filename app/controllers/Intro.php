18162c047928a4e1274ab6a87e41fbc5a7d82a51
clement <clement.salomon@etu.univ-amu.fr>
2023-10-24 11:46:25 +0200

    Ecriture de tout les dialogues (dans des variables)

--- app/controllers/Intro.php
+++ app/controllers/Intro.php
@@ -9,7 +9,7 @@ class IntroController
     public function execute(): void
     {
         $text_intro = "Avec l'émergence du numérique et de l'intelligence artificielles, les Fakes News sont de plus en présentes et prennent facilement de l'ampleur. Elles sont dans tout les domaines, et touchent tout les âges. <br> Le but de ce serious game est de vous apprendre de manière ludique à les repérer pour mieux vous en protéger. <br> Je vais vous guider tout au long du jeu, ensemble, rétablissons la vérité !";
-        $Q_adult = "Hey, <br> j’ai pris l’habitude de lire le journal dans le parc après mon boulot. <br> Cependant, j’ai récemment remarqué que de plus en plus d’articles sonnaient “faux” ou peu fiables. <br> Pourrais tu m’aider à les trouver ?";
+        $text_outro = "bravo";
 
         $dataSpeaker = [
             'character_type' => 'me',
