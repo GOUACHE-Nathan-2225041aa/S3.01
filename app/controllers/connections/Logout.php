dd7ceb321d1934a554b3030e6de7d7998ea87aa2
Edouard-Pon <edouard.ponomarenko@gmail.com>
2023-12-14 23:35:06 +0100

    Updated Logout controller

--- app/controllers/connections/Logout.php
+++ app/controllers/connections/Logout.php
@@ -6,6 +6,21 @@ class Logout
 {
     public function execute(): void
     {
+        $_SESSION = array();
+
+        if (ini_get('session.use_cookies')) {
+            $params = session_get_cookie_params();
+            setcookie(
+                session_name(),
+                '',
+                time() - 42000,
+                $params['path'],
+                $params['domain'],
+                $params['secure'],
+                $params['httponly']
+            );
+        }
+
         session_destroy();
         header('Location: /');
         exit();
