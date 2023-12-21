2a22ec8a61ef478aeadc778218c2e59a02d14128
victorSejourne <victor.sejourne@etu.univ-amu.fr>
2023-12-21 23:25:34 +0100

    Recovery css

--- app/views/connections/Recovery.php
+++ app/views/connections/Recovery.php
@@ -11,7 +11,8 @@ class Recovery
         ob_start();
 ?>
 <main>
-    <form method="POST" action="">
+    <h1>Forgot Password ?</h1>
+    <form method="POST" action="" id="test">
         <?php if (isset($_SESSION['errorMessage'])): ?>
             <div class="errorMessage">
                 <?= $_SESSION['errorMessage'] ?>
@@ -31,14 +32,15 @@ class Recovery
         <input type="password" name="passwordConfirmation" id="passwordConfirmation">
 
         <?php endif; ?>
-
-        <a href="/login">Already have an account</a>
-        <a href="/signup">Create Account</a>
-
         <button type="submit" name="<?= $isEmailVerified ? 'recovery' : 'recovery_email_verification'; ?>">Reset Password</button>
+
+        <div id="div-a">
+            <a href="/login">Already have an account</a>
+            <a href="/signup">Create Account</a>
+        </div>
     </form>
 </main>
 <?php
-        (new Layout('FakeGame - Recovery', ob_get_clean(), "Recovery.css"))->show();
+        (new Layout('FakeGame - Recovery', ob_get_clean(), 'Recovery'))->show();
     }
 }
