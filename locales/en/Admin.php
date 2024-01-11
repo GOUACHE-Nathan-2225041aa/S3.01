54be82f2f6886661c105049303e4bf41c8fd33b6
NathanGOUACHEG22017449 <nathan.gouache@etu.univ-amu.fr>
2024-01-11 16:48:00 +0100

    Admin translation En

---
+++ locales/en/Admin.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'title' => 'Title',
+    'image' => 'Image',
+    'answer' => 'Answer',
+    'hint' => 'Hint',
+    'description' => 'Description',
+    'source' => 'Source',
+    'language' => 'Language',
+    'audio' => 'Audio',
+];
