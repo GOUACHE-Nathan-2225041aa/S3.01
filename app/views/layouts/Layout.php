0bfc6c3caafa9dffff18389279a314c68e3b28d0
NathanGOUACHEG22017449 <nathan.gouache@etu.univ-amu.fr>
2024-01-17 20:21:14 +0100

    Changed the line of the logo

--- app/views/layouts/Layout.php
+++ app/views/layouts/Layout.php
@@ -14,12 +14,12 @@ class Layout
 <!doctype html>
 <html lang="fr">
 <head>
-    <link rel="icon" type="image/x-icon" href="/assets/images/divers/Logo_MarsActu_SeriousGame.png">
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?= $this->title ?></title>
+    <link rel="icon" type="image/x-icon" href="/assets/images/divers/Logo_MarsActu_SeriousGame.png">
     <link rel="stylesheet" href="/assets/stylesheets/main.css">
     <?php if ($this->stylesheet !== '') { ?>
     <link rel="stylesheet" href="/assets/stylesheets/<?= $this->stylesheet ?>.css">
