81dff9fc2f37ffe4f81c6c974d5fdd9c7370e250
NathanGOUACHEG22017449 <nathan.gouache@etu.univ-amu.fr>
2024-03-04 19:05:41 +0100

    Réorganisation du menu

--- app/views/partials/Header.php
+++ app/views/partials/Header.php
@@ -15,20 +15,14 @@ class Header
     <nav class="navbar">
         <a href="/"><?= $loc['intro'] ?></a>
         <a href="/home"><?= $loc['home'] ?></a>
-        <a href="/login"><?= $loc['login'] ?></a>
-        <a href="/signup"><?= $loc['signup'] ?></a>
-        <a href="/admin"><?= $loc['admin'] ?></a>
-        <div>
-            <label for="language-selector"><?= $loc['language'] ?></label>
-            <select id="language-selector">
-                <option <?= $_SESSION['language'] === 'fr' ? 'selected' : ''?> value="fr"><?= $loc['fr'] ?></option>
-                <option <?= $_SESSION['language'] === 'en' ? 'selected' : ''?> value="en"><?= $loc['en'] ?></option>
-            </select>
-        </div>
-        <div>
-            <button id="save-progress"><?= $loc['saveProgress'] ?></button>
-        </div>
+        <a href="/login"><?= $loc['account'] ?></a>
+        <button id="save-progress"><?= $loc['saveProgress'] ?></button>
         <button id="load-progress"><?= $loc['loadProgress'] ?></button>
+        <a href="/admin"><?= $loc['admin'] ?></a>
+        <select id="language-selector">
+            <option <?= $_SESSION['language'] === 'fr' ? 'selected' : ''?> value="fr"><?= $loc['fr'] ?></option>
+            <option <?= $_SESSION['language'] === 'en' ? 'selected' : ''?> value="en"><?= $loc['en'] ?></option>
+        </select>
     </nav>
 </header>
 <?php
