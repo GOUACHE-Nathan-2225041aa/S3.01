6f4a531071668cd4e4a9f4860eed1f9659b1dd1e
Edouard-Pon <edouard.ponomarenko@gmail.com>
2024-03-07 09:17:25 +0100

    Update DataService

--- app/services/DataService.php
+++ app/services/DataService.php
@@ -13,4 +13,47 @@ class DataService
         }
         return $gameData;
     }
+
+    public static function buildLocalizationData(array $postData): array
+    {
+        $language = htmlspecialchars($postData['language']);
+        return [
+            [
+                'field' => 'title',
+                'language' => $language,
+                'text' => htmlspecialchars($postData['title']),
+            ],
+            [
+                'field' => 'hint',
+                'language' => $language,
+                'text' => htmlspecialchars($postData['hint']),
+            ],
+            [
+                'field' => 'description',
+                'language' => $language,
+                'text' => htmlspecialchars($postData['description']),
+            ],
+        ];
+    }
+
+    public static function buildLocalizationDataLang(array $postData, string $lang): array
+    {
+        return [
+            [
+                'field' => 'title',
+                'language' => $lang,
+                'text' => htmlspecialchars($postData['title-' . $lang]),
+            ],
+            [
+                'field' => 'hint',
+                'language' => $lang,
+                'text' => htmlspecialchars($postData['hint-' . $lang]),
+            ],
+            [
+                'field' => 'description',
+                'language' => $lang,
+                'text' => htmlspecialchars($postData['description-' . $lang]),
+            ],
+        ];
+    }
 }
